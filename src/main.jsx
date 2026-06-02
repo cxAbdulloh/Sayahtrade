35638808782c10575e77b3e2d58f1977745621de
Abdulloh <abdurahmonovabdulloh379@gmail.com>
2026-06-02 15:20:42 +0300

    telegram mini app setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Telegram Mini App
+const tg = window.Telegram.WebApp;
+tg.ready();
+tg.expand();
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
